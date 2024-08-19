fa166c2a4d80f82a0d2836cce2c5c33c90e8f0e5
Ayush Pawar <ayushap1105@gmail.com>
2024-08-19 21:21:23 +0530

    Day 4 learning uploaded

---
+++ Day 4/questions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    //! check whether the entered year is a leap year or not
+    // int year;
+    // cin >> year;
+
+    // if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+    //     cout << "it's a leap year" << year << endl;
+    // }
+    // else{
+    //     cout << "it's not a leap year" << year << endl;
+    // }
+
+
+    //! sum of n numbers
+    // int n;
+    // cin >> n;
+    // cout << "Sum of n numbers is : " << (n*(n+1)/2) << endl;
+
+    //* Conversion from decimal to binary
+    int n = (1%2);
+    cout << n << endl;
+    
+    return 0;
+}
