2b7c5bc24acb6a950acc1e4c2e39926dba3cb253
Ayush Pawar <ayushap1105@gmail.com>
2024-08-20 20:57:45 +0530

    Day 4 learning

---
+++ Day 4/decimal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// int decimalToBinaryMethod1(int n){
+//     // Division Method
+//     while(n > 0){
+//         int bit = n % 2;
+//         cout << bit << endl;
+//         n = n / 2;
+//     }
+//     return 0;
+// }
+
+
+int main(){
+
+    //! error after 11 
+    // int n;
+    // cin >> n;
+    // int binaryNo = 0;
+    // int power = 0;
+    // while(n > 0){
+    //     int bit = n % 2;
+    //     binaryNo  = bit * pow(10,power++) + binaryNo;
+    //     n = n / 2;
+       
+    // }
+    // cout << binaryNo << endl;
+
+
+    //* Binary to decimal
+    return 0;
+}
