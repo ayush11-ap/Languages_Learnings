920117d4b5cef0d0eda9dc50dc2942b22ef6661f
Ayush Pawar <ayushap1105@gmail.com>
2024-08-22 20:56:57 +0530

    Day 6 learning of C++

--- Day 6/functions.cpp
+++ Day 6/functions.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void printName(){
+    for(int i = 0; i < 3; i++){
+        cout << "ASAN" << endl;
+    }
+}
+int sum(int a, int b);
+
+
+    // 
 int main(){
 
+    printName();
+    int ans = sum(2, 11);
+    cout << "Sum is : " << ans << endl;
+
     return 0;
+}
+
+int sum(int a, int b){
+    int total = a + b;
+    return total;
 }
