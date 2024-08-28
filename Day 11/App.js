9f2ea2254710fa8ad13a8b4c8006390d59923c87
Ayush Pawar <ayushap1105@gmail.com>
2024-08-28 21:22:46 +0530

    Day 11 Learning

--- Day 11/App.js
+++ Day 11/App.js
@@ -1,1 +1,23 @@
-console.log(process.argv);
+//! process.argv in Node.js is an array that contains the command-line arguments passed when the Node.js process was started.
+//* process.argv provides the path of C:// & D:// drive in the form of an array
+console.log(process.argv[0]);
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+})
+
+rl.question("Who are a you", (ans) => {
+    console.log(`I'm a ${ans}`);
+
+    rl.question("Who is ur age", (age) => {
+        console.log(`I'm a ${age}`);
+        
+        rl.close();
+    })
+    
+})
+
