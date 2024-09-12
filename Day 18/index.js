32275d8ca74cdcd285c9ecae3ac27de9709e1029
Ayush Pawar <ayushap1105@gmail.com>
2024-09-12 21:16:17 +0530

    Day 18 learning

--- Day 18/index.js
+++ Day 18/index.js
@@ -11,30 +11,49 @@
 // O(n + 1) =>  O(n)
 
 // prototype
-// function Student(name, rollNo, makrs){
-//     this.name = name;
-//     this.rollNo = rollNo;
-//     this.makrs= makrs;
-// }
+function Student(name, rollNo, makrs){
+    this.name = name;
+    this.rollNo = rollNo;
+    this.makrs= makrs;
 
-// Student.prototype.getMarks = function(){
-//     console.log(this.makrs);
+    // this.getMarks = function(){
+    //     console.log(this.makrs);
+        
+    // }
+}
+
+Student.prototype.getMarks = function(){
+    console.log(this.makrs);
     
-// }
+}
 
-// let s1 = new Student("ayush", 11, 99);
-// let s2 = new Student("piyush", 11, 89);
+let s1 = new Student("ayush", 11, 99);
+let s2 = new Student("piyush", 11, 89);
 
-// console.log(s1);
-// s1.getMarks();
+console.log(s1);
+//s1.getMarks();
 
-// console.log(s2);
+console.log(s2);
+
+// Every is js object
+
+function add1(a, b){
+    console.log(a + b);
+}
+
+add1.value = "Ayush";
+console.log(add1.value);
+
+
 
 
 
 let name = "Ayush      ";
 
-String.prototype.actualLength = function(){
+console.log(name.length);
+
+
+Array.prototype.actualLength = function(){
     console.log(`Actual length is: ${this.trim().length}`);
 }
 
